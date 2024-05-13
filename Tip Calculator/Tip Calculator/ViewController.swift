6c324bb105d75771ea6e33c32f31fae01d7a2629
Raymond Kim <115915118+rayk1m02@users.noreply.github.com>
2024-05-13 16:44:16 -0700

    Add files via upload

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Raymond Kim on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
